394e23a12030ac2fb0456f7c4bb2cfdbc323105e
Garcez <allan.f.garcez@gmail.com>
2019-01-29 21:30:26 -0200

    People class domain

---
+++ Domain/People.cs
@@ -0,0 +1,10 @@
+namespace dotnetTest.Domain
+{
+    public class People
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Status Status { get; set; }
+    }
+}
