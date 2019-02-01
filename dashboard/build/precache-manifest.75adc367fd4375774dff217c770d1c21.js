884967595121735d71de2e0affc4ed7c7584e92c
Garcez <allan.f.garcez@gmail.com>
2019-02-01 15:24:36 -0200

    frontend

---
+++ dashboard/build/precache-manifest.75adc367fd4375774dff217c770d1c21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5acbbc71b21e35e147f",
+    "url": "./static/css/main.8fb99549.chunk.css"
+  },
+  {
+    "revision": "a5acbbc71b21e35e147f",
+    "url": "./static/js/main.a5acbbc7.chunk.js"
+  },
+  {
+    "revision": "40feb3e205eb14d91967",
+    "url": "./static/js/1.40feb3e2.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9801e416341850a784ad0dcd67fa7a30",
+    "url": "./index.html"
+  }
+];
